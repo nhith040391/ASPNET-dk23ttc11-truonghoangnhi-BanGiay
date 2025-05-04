5fba90d2d3a4002676c2c5484edc039c1a203aa1
nhith040391 <nhith040391@sv-onuni.edu.vn>
2025-05-04 12:21:36 +0700

    WebBanGiay

---
+++ BanGiay/Model/DAO/SliderDAO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EF;
+using PagedList;
+
+namespace Model.DAO
+{
+    public class SliderDAO
+    {
+        NConnect db = null;
+        public SliderDAO()
+        {
+            db = new NConnect();
+        }
+
+        public List<SLIDE> GetListActive()
+        {
+            return db.SLIDEs.Where(x => x.TrangThai == 2).OrderByDescending(x => x.ID).ToList();
+        }
+    }
+}
